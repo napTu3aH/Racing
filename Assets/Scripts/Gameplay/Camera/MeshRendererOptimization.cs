40b2bf759e35de74cffd87f46f32af92fb5d7604
Salazar <napti3an95@bk.ru>
2015-11-29 18:42:00 +0600

    Mesh optimizations
    Added mesh optimizations

---
+++ Assets/Scripts/Gameplay/Camera/MeshRendererOptimization.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshRendererOptimization : MonoBehaviour {
+    public Transform _MeshRoot;
+    public bool _Visible;
+
+    MeshRenderer[] _Renderers;
+
+    void Awake()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        if (_MeshRoot)
+        {
+            _Renderers = _MeshRoot.GetComponentsInChildren<MeshRenderer>();
+        }
+    }
+
+    void OnBecameVisible()
+    {
+        _Visible = true;
+        Visibling();
+    }
+
+    void OnBecameInvisible()
+    {
+        _Visible = false;
+        Visibling();
+    }
+
+    void Visibling()
+    {
+        if (_Renderers.Length > 0)
+        {
+            for (int i = 0; i < _Renderers.Length; i++)
+            {
+                _Renderers[i].enabled = _Visible;
+            }
+        }
+    }
+}
